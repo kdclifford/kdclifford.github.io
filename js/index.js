22a8375f52db5d909c221bc4b2a0832976fbb28a
kyle clifford <kdclifford@icloud.com>
2023-08-16 18:01:51 +0100

    Updatated project links and started post processing page

--- js/index.js
+++ js/index.js
@@ -1,49 +1,9 @@
-function dynamicImg(element, preview) {
-    element.setAttribute('src', preview + '.gif');
-};
-
-function staicImg(element, preview) {
-    element.setAttribute('src', preview);
-};
-
-
-function addImageLink(link, image, name, elementId) {
-    const a = document.createElement("a");
-    a.setAttribute("class", "tabLinks");
-    a.setAttribute("href", link);
-    a.setAttribute("target", "_blank");
-
-    const div = document.createElement("div");
-    div.setAttribute("class", "image");
-
-    const img = document.createElement("img");
-    img.setAttribute("alt", name);
-    img.setAttribute("src", image);
-    img.setAttribute("title", name);
-
-    div.appendChild(img);
-
-    a.appendChild(div);
-
-    const element = document.getElementById(elementId);
-    element.appendChild(a);
-};
-
-function addPageLink(link, text, elementId) {
-    const a = document.createElement("a");
-    a.setAttribute("class", "pageLink");
-    a.setAttribute("href", link);
-    a.innerHTML = text;
-
-    const element = document.getElementById(elementId);
-    element.appendChild(a);
-};
-
 window.onload = function () {
     addImageLink("https://github.com/kdclifford", "./Assets/github.png", "Github", "navigation");
     addImageLink("https://www.linkedin.com/in/kyle-clifford-7474b4206", "./Assets/linkedin.png", "Linkedin", "navigation");
-    addPageLink("mailto: kdclifford@icloud.com", "kdclifford@icloud.com", "navigation");
-    addPageLink("./index.html#about", "About", "navigation");
-    addPageLink("./index.html#projects", "Work", "navigation");
-    addPageLink("./index.html#contact", "Contact", "navigation");
+    addPageLink("mailto: kdclifford@icloud.com", "kdclifford@icloud.com", "pageLink email", "navigation");
+    addPageLink("./index.html#about", "About", "pageLink", "navigation");
+    addPageLink("./index.html#projects", "Work", "pageLink", "navigation");
+    addPageLink("./index.html#contact", "Contact", "pageLink", "navigation");
+
 }
---
+++ js/postprocessing.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    addImageLink("https://github.com/kdclifford", "./Assets/github.png", "Github", "navigation");
+    addImageLink("https://www.linkedin.com/in/kyle-clifford-7474b4206", "./Assets/linkedin.png", "Linkedin", "navigation");
+    addPageLink("mailto: kdclifford@icloud.com", "kdclifford@icloud.com", "pageLink email", "navigation");
+    addPageLink("./index.html", "Home", "pageLink", "navigation");
+    addPageLink("./PostProcessing.html#intro", "Intro", "pageLink", "navigation");
+    addPageLink("./PostProcessing.html#features", "Features", "pageLink", "navigation");
+    addPageLink("./PostProcessing.html#effects", "Effects", "pageLink", "navigation");
+    addPageLink("./PostProcessing.html#contact", "Contact", "pageLink", "navigation");
+
+}
