fd75932e8ea4fe654505046285cd8575ad27f367
kyle clifford <kdclifford@icloud.com>
2023-08-17 14:02:01 +0100

    Added youtube link

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     addImageLink("https://github.com/kdclifford", "./Assets/github.png", "Github", "navigation");
     addImageLink("https://www.linkedin.com/in/kyle-clifford-7474b4206", "./Assets/linkedin.png", "Linkedin", "navigation");
+    addImageLink("https://www.youtube.com/@bleh1462/videos", "./Assets/youtube.png", "Youtube", "navigation");
     addPageLink("mailto: kdclifford@icloud.com", "kdclifford@icloud.com", "pageLink email", "navigation");
     addPageLink("./index.html#about", "About", "pageLink", "navigation");
     addPageLink("./index.html#projects", "Work", "pageLink", "navigation");
