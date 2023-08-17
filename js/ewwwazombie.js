9163fba267dcce133112b69d6b1f4d039291b5e7
kyle clifford <kdclifford@icloud.com>
2023-08-17 20:46:57 +0100

    Added ewww a Zombie page

---
+++ js/ewwwazombie.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    addImageLink("https://github.com/kdclifford", "./Assets/github.png", "Github", "navigation");
+    addImageLink("https://www.linkedin.com/in/kyle-clifford-7474b4206", "./Assets/linkedin.png", "Linkedin", "navigation");
+    addImageLink("https://www.youtube.com/@bleh1462/videos", "./Assets/youtube.png", "Youtube", "navigation");
+    addImageLink("https://stalwartlist.itch.io/ewww-a-zombie", "./Assets/itch.io.png", "Itch.io", "navigation");
+    addPageLink("mailto: kdclifford@icloud.com", "kdclifford@icloud.com", "pageLink email", "navigation");
+    addPageLink("./index.html", "Home", "pageLink", "navigation");
+    addPageLink("./EwwwAZombie.html#intro", "Intro", "pageLink", "navigation");
+    addPageLink("./EwwwAZombie.html#contact", "Contact", "pageLink", "navigation");
+}
